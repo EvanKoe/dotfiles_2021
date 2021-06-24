52c86ad0e715c6132c541f67b3daf219c14f0376
Evan Koehler <72010109+EvanKoe@users.noreply.github.com>
2021-06-25 00:15:47 +0200

    Delete mr_clean.sh

--- mr_clean.sh
+++
@@ -1,5 +0,0 @@
-find -type f -name "*.gc*" -exec rm {} +
-find -type f -name "vgcore*" -exec rm {} +
-find -type f -name "#*#" -exec rm {} +
-find -type f -name "~*" -exec rm {} +
-echo "Home directory has been cleaned"
